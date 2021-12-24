777f3adf2f99eef295d307aed637f992b145b433
serhii <sergiydotsenko@gmail.com>
2021-12-24 17:12:52 +0200

    added ledger support

--- src/pages/verifybot.tsx
+++ src/pages/verifybot.tsx
@@ -5,9 +5,25 @@ import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-r
 import { Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 import axios from "axios";
 
+
 import { v4 as uuidv4 } from 'uuid';
 
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
+
+import { useDisclosure } from '@chakra-ui/react'
+
 export default function VerifyBot() {
+
+    // ledger stuff
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     const { connection } = useConnection();
     const { publicKey } = useWallet();
@@ -20,7 +36,6 @@ export default function VerifyBot() {
         }).join('')
     }
 
-
     const urlParams = new URLSearchParams(window.location.search);
     const discordTokenValue = urlParams.get('token');
 
@@ -31,6 +46,16 @@ export default function VerifyBot() {
     } else {
         // discordToken = true;
         window.location.href = "https://api.soltracker.io/verify_user_discord"
+    }
+
+    function showDeclinedAlert() {
+        toast({
+            title: "Message sign request were declined",
+            position: "top-right",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+        })
     }
 
     function verifyBot() {
@@ -90,75 +115,11 @@ export default function VerifyBot() {
                     })
                 })
             }).catch(function (err) {
-                toast({
-                    title: "Message sign request were declined: "+err.message,
-                    position: "top-right",
-                    status: "warning",
-                    duration: 5000,
-                    isClosable: true,
-                })
-                console.log('failed to sign a message' + err)
+                onOpen();
             })
         } else {
             if (signTransaction != null) {
-
-                connection.getRecentBlockhash('finalized').then(resp => {
-                    const transaction = new Transaction().add(
-                        SystemProgram.transfer({
-                            fromPubkey: publicKey as PublicKey,
-                            toPubkey: publicKey as PublicKey,
-                            lamports: 100,
-                        })
-                    );
-
-                    transaction.feePayer = publicKey as PublicKey
-
-                    transaction.recentBlockhash = resp.blockhash
-                    const serializedTx = transaction.serializeMessage()
-
-                    signTransaction(transaction).then(function (signedTx) {
-
-                        const verifyNewUser = {
-                            sig_uid: toHexString(serializedTx),
-                            sig_hex: toHexString(signedTx.signatures[0].signature as Buffer),
-                            sig_tpl_ver: "ledger_tx",
-                            pubkey: publicKey.toBase58(),
-                            discord_token: discordTokenValue,
-                            discord_token_hash: "",
-                            timestamp: ts
-                        }
-        
-                        axios.post('https://sol.catchmetech.com/verify/new', verifyNewUser).then((response) => {
-        
-                            toast({
-                                title: "Shortly you'll get discord roles",
-                                position: "top-right",
-                                status: "success",
-                                duration: 5000,
-                                isClosable: true,
-                            })
-        
-                        }).catch((e) => {
-        
-                            toast({
-                                title: "Error verifying: " + e.response.data.msg,
-                                position: "top-right",
-                                status: "error",
-                                duration: 5000,
-                                isClosable: true,
-                            })
-                        })
-
-                    });
-                }).catch(e => {
-                    toast({
-                        title: "Solana network error. try again later",
-                        position: "top-right",
-                        status: "warning",
-                        duration: 5000,
-                        isClosable: true,
-                    })
-                });
+                signTransactionMethod();
             } else {
                 toast({
                     title: "Unable to verify wallet ownership",
@@ -171,8 +132,119 @@ export default function VerifyBot() {
         }
     }
 
+    function signTransactionMethod() {
+        if (signTransaction != null && publicKey != null) {
+
+            const ts = Math.floor((new Date()).getTime() / 1000)
+
+            connection.getRecentBlockhash('finalized').then(resp => {
+                const transaction = new Transaction().add(
+                    SystemProgram.transfer({
+                        fromPubkey: publicKey as PublicKey,
+                        toPubkey: publicKey as PublicKey,
+                        lamports: 100,
+                    })
+                );
+
+                transaction.feePayer = publicKey as PublicKey
+
+                transaction.recentBlockhash = resp.blockhash
+                const serializedTx = transaction.serializeMessage()
+
+                signTransaction(transaction).then(function (signedTx) {
+
+                    const verifyNewUser = {
+                        sig_uid: toHexString(serializedTx),
+                        sig_hex: toHexString(signedTx.signatures[0].signature as Buffer),
+                        sig_tpl_ver: "ledger_tx",
+                        pubkey: publicKey.toBase58(),
+                        discord_token: discordTokenValue,
+                        discord_token_hash: "",
+                        timestamp: ts
+                    }
+
+                    axios.post('https://sol.catchmetech.com/verify/new', verifyNewUser).then((response) => {
+
+                        toast({
+                            title: "Shortly you'll get discord roles",
+                            position: "top-right",
+                            status: "success",
+                            duration: 5000,
+                            isClosable: true,
+                        })
+
+                    }).catch((e) => {
+
+                        toast({
+                            title: "Error verifying: " + e.response.data.msg,
+                            position: "top-right",
+                            status: "error",
+                            duration: 5000,
+                            isClosable: true,
+                        })
+                    })
+
+                }).catch(e => {
+                    toast({
+                        title: "Unable to confirm wallet ownership: "+e.message,
+                        position: "top-right",
+                        status: "warning",
+                        duration: 5000,
+                        isClosable: true,
+                    })
+                });
+            }).catch(e => {
+                toast({
+                    title: "Solana network error. try again later",
+                    position: "top-right",
+                    status: "warning",
+                    duration: 5000,
+                    isClosable: true,
+                })
+            });
+        } else {
+            toast({
+                title: "Something got wrong. Contact support",
+                position: "top-right",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+            })
+        }
+    }
+
+    function usingLedger() {
+        onClose();
+        signTransactionMethod();
+    }
+
+    function notUsingLedger() {
+        onClose();
+        showDeclinedAlert();
+    }
+
     if (discordToken) {
         return <Box p={6} marginTop="20px" >
+
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Ooops. Do you use ledger bound wallet?</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <Text fontSize="m">In that case we ask you to sign transaction to confirm you own a wallet by pressing the green button below.</Text>
+                        {/* <Text fontSize="xs">Transaction sends a small ammount of sol from your wallet to your wallet</Text> */}
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='green' mr={3} onClick={usingLedger}>
+                            Yes, i'm using ledger
+                        </Button>
+                        <Button onClick={notUsingLedger}>No</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+
             <Text
                 bgGradient="linear(to-l, #7928CA, #FF0080)"
                 bgClip="text"
